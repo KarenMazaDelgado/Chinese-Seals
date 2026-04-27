94439116ff59ed25004fe5d8aeb782470b274362
KarenMazaDelgado <karenmaza20300@gmail.com>
2026-04-27 14:41:31 -0400

    added home button and all seals as targets

--- scanner/src/app.js
+++ scanner/src/app.js
@@ -1,7 +1,11 @@
 const onxrloaded = () => {
   XR8.XrController.configure({
     imageTargetData: [
-      require('../image-targets/stamp1.json'),
+      require('../image-targets/stamp1/stamp1.json'),
+      require('../image-targets/stamp2/stamp2.json'),
+      require('../image-targets/stamp3/stamp3.json'),
+      require('../image-targets/stamp4/stamp4.json'),
+      require('../image-targets/stamp5/stamp5.json'),
     ],
   })
 }
